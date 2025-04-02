a8fd6eba438a199b828bc67a46ef081d14a48102
MukisaSam <mukisasamuel2020@gmail.com>
2025-04-02 13:43:04 -0700

    Delete HTMLschoolAssignment directory

--- HTMLschoolAssignment/Admission/admissions.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Admissions page loaded successfully.');
-  
-    // Example: Add interactivity for a future feature
-    const ctaButton = document.querySelector('.cta .btn');
-    ctaButton.addEventListener('click', () => {
-      alert('Redirecting to the Contact Us page...');
-    });
-  });
